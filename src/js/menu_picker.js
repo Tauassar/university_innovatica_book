f02bf32db0d8d5a20f26f478eb214390c416268b
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-04-23 21:00:08 +0600

    navigation fix

--- src/js/menu_picker.js
+++ src/js/menu_picker.js
@@ -1,5 +1,8 @@
 
 function set_current_menu_item() {
-    console.log('set_current_menu_item');
+    const path = `[href="${window.location.pathname}"]`;
+    let get = document.querySelectorAll(path);
+    console.log("The element fetched by href attribute is:", get);
+    get[0].classList.add("is-active");
 }
 set_current_menu_item()
